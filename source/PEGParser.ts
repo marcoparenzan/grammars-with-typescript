984a20724cd6423d0f56a256a104fdc2ce14d284
Marco Parenzan <marco.parenzan@libero.it>
2014-05-17 07:09:06 +0200

    Added first parse with sense

--- source/PEGParser.ts
+++ source/PEGParser.ts
@@ -1,6 +1,7 @@
 /// <reference path="PEGToken.ts" />
 /// <reference path="PEGLexer.ts" />
 /// <reference path="PEGProduction.ts" />
+/// <reference path="PEGParseAction.ts" />
 
 class PEGParser {
 	_lexer: PEGLexer;
@@ -43,7 +44,68 @@ class PEGParser {
 		return this._tokens.splice(0, n);		
 	}
 	
-	parse(): void {
-		
+    parse(): PEGProduction[] {
+        var productions: PEGProduction[] = [];
+        var temp: PEGToken[];
+
+        // production loop
+        while (true)
+        {
+            if (!this._can_be(["IDENTIFIER", "EQUALS"])) throw Error("Expected production name");
+            temp = this._capture(2);
+            var production_name:string = temp[0].value;
+
+            // production alternatives loop
+            var production_empty = true;
+            var current_action: PEGParseAction = {};
+            while (true)
+            {
+                if (this._can_be(["COMMENT"]))
+                {
+                    this._capture(1);
+                    continue;
+                }
+                else if (this._can_be(["QUOTE"])) {
+                    temp = this._capture(1);
+                    var quote = temp[0].value;
+
+                    production_empty = false;
+                    continue;
+                }
+                else if (this._can_be(["IDENTIFIER", "COLON", "IDENTIFIER"]))
+                {
+                    temp = this._capture(3);
+                    var child_name = temp[0].value;
+                    var selected_production = temp[2].value;
+
+                    production_empty = false;
+                    continue;
+                }
+                else if (this._can_be(["IDENTIFIER"])) {
+                    temp = this._capture(1);
+                    var selected_production = temp[0].value;
+
+                    production_empty = false;
+                    continue;
+                }
+
+                if (this._can_be(["JAVASCRIPT"]))
+                {
+                    temp = this._capture(1);
+                    var javascript = temp[0].value;
+                }
+
+                if (this._can_be(["PIPE"])) {
+                    this._capture(1);
+                    continue;
+                }
+                else if (this._can_be(["IDENTIFIER", "EQUALS"])) {
+                    break;
+                }
+            }
+            if (production_empty) throw Error("Production is empty");
+        }
+
+        return productions;
 	}
 }
